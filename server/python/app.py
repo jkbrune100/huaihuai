5d9c4077929718e57a40a3b86090d26cfab276a5
Raspberry Pi User <raspi1@ostfalia.robotics.de>
2022-11-01 10:13:38 +0000

    remote

--- server/python/app.py
+++ server/python/app.py
@@ -8,7 +8,7 @@ import RPi.GPIO as GPIO
 
 app = Flask(__name__)
 
-home_positions = [ ("j1", 0, 90/180.0 * math.pi), ("j2", 3, 90.0/180.0 * math.pi), ("j3", 4, 90.0/180.0*math.pi), ("j4", 7, 90.0/180.0 * math.pi) ]
+home_positions = [ ("j1", 0, 90/180.0 * math.pi), ("j2", 3, 90.0/180.0 * math.pi), ("j3", 4, 90.0/180.0*math.pi), ("j4", 7, 80.0/180.0 * math.pi) ]
 
 def find_joint( jname, joints ):
     ret = None
@@ -39,8 +39,8 @@ def joints( ):
     print(f"Setting joints: j2 = {j2}, deg.{j2/math.pi * 180.0}")
     print(f"Setting joints: j3 = {j3}, {j3/math.pi * 180.0}")
     print(f"Setting joints: j4 = {j4}, {j4/math.pi * 180.0}")
-
-    joints = [j1, j2, j3, j4]
+    
+    joints = [j1, -j2, j3, j4]
     for i,jname in enumerate([ "j1", "j2", "j3", "j4" ]):
         joint = find_joint( jname, home_positions )
         if joint:
@@ -57,9 +57,11 @@ def joints( ):
         jname, joint, home = h
                 
         val = convert_angle_to_pwm( joints[i] )
-        if val is not None:        
+
+        if val is not None:     
+            print(joint,val)   
             set_servo_pulse( pwm, joint, val )
-            
+    print()
     return f"Ok"
 
 @app.route('/test/<param>', methods=['GET', 'POST'])
@@ -68,25 +70,25 @@ def test( param ):
     return "ok"
 
 # Einstellen der Minimal- und Maximal-Pulslaengen
-servo_min = 150  # Minimale Pulslaenge
-servo_max = 600  # Maximale Pulslaenge
+servo_min_j1 = 260  # Minimale Pulslaenge of the feet joint
+servo_max_j1 = 445  # Minimale Pulslaenge of the feet joint
+servo_min = 120  # Minimale Pulslaenge of else joints
+servo_max = 500  # Maximale Pulslaenge of else joints
 
 # Hilfsfunktion
 def set_servo_pulse(pwm, channel, pulse):
-    pulse_length = 1000000 
-    pulse_length /= 50     
-    print('{0}us per period'.format(pulse_length))
-    pulse_length /= 4096     
-    print('{0}us per bit'.format(pulse_length))
-    pulse *= 1000
-    print(pulse_length)
-    pulse /= pulse_length
-    print(pulse)
-    pulse = round(pulse)
-    print(pulse)
+
     pulse = int(pulse)
+    
+    if channel == 7:
+        max(pulse, servo_min_j1)
+        min(pulse, servo_max_j1)
+    else:
+        max(pulse, servo_min)
+        min(pulse, servo_max)        
     print (pulse)
-    pwm.set_pwm(channel, 0, pulse-25)
+    
+    pwm.set_pwm(channel, 0, pulse)
 
 def normalize_angle( angle ):
     while(angle < 0):
@@ -98,11 +100,14 @@ def normalize_angle( angle ):
 def convert_angle_to_pwm( angle ):
     ret = None
     angle = normalize_angle( angle )
-    if angle >= 0.0 and angle < 2 * math.pi:
-        p_width = 650 #600 - 150
-        pulse = 0 + angle/(2*math.pi)*p_width
+    print("angle", angle/math.pi*180)
+    
+    if angle >= 0 and angle <  math.pi:
+        p_width = servo_max - servo_min #600 - 150
+        pulse = servo_min + angle/(math.pi)*p_width
         ret = pulse
-    return ret
+    
+    return ret 
 
 pwm = Adafruit_PCA9685.PCA9685(address=0x41)
 
@@ -119,8 +124,8 @@ def create_app():
     for h in home_positions:
         jname, joint, home = h        
         val = convert_angle_to_pwm( home )
-        if val is not None:        
-            set_servo_pulse( pwm, joint, home )
+#        if val is not None:        
+#            set_servo_pulse( pwm, joint, home )
             
     app.run( host="0.0.0.0", port=8080 )
 
